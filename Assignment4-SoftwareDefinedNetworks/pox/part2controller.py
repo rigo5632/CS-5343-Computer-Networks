aee2967526e626c90d0a0126bf6fd5b3b25761ea
rigo5632 <rmquiroz5@gmail.com>
2020-11-12 16:07:10 -0500

    added icmp and arp firewall

--- Assignment4-SoftwareDefinedNetworks/pox/part2controller.py
+++ Assignment4-SoftwareDefinedNetworks/pox/part2controller.py
@@ -20,20 +20,23 @@ class Firewall (object):
     connection.addListeners(self)
 
     #add switch rules here only
+    # 0x806 
     self.rules = [
-      {'dl_type':0x806,'idle_timeout':1000,'action':of.OFPP_FLOOD,'dl_dst':None},
-      {'dl_dst':EthAddr('00:00:00:00:00:01'),'idle_timeout':1000,'action':1,'dl_type':0x800},
-      {'dl_dst':EthAddr('00:00:00:00:00:02'),'idle_timeout':1000,'action':2,'dl_type':0x800},
-      {'dl_dst':EthAddr('00:00:00:00:00:03'),'idle_timeout':1000,'action':3,'dl_type':0x800},
-      {'dl_dst':EthAddr('00:00:00:00:00:04'),'idle_timeout':1000,'action':4,'dl_type':0x800},      
+      #ICMP Firewall rule
+      {'dl_dst':None,'idle_timeout':1000,'action':of.OFPP_FLOOD,'dl_type':0x806,'nw_proto':None},
+      {'dl_dst':EthAddr('00:00:00:00:00:01'),'idle_timeout':1000,'action':1,'dl_type':0x800,'nw_proto':1},
+      {'dl_dst':EthAddr('00:00:00:00:00:02'),'idle_timeout':1000,'action':2,'dl_type':0x800,'nw_proto':1},
+      {'dl_dst':EthAddr('00:00:00:00:00:03'),'idle_timeout':1000,'action':3,'dl_type':0x800,'nw_proto':1},
+      {'dl_dst':EthAddr('00:00:00:00:00:04'),'idle_timeout':1000,'action':4,'dl_type':0x800,'nw_proto':1},  
     ]
 
   def _handle_ConnectionUp(self, event):
     for rule in self.rules:
       msg = of.ofp_flow_mod()
-      if rule['dl_dst']: msg.match.dl_dst = rule['dl_dst']
-      msg.match.dl_type = rule['dl_type']
-      msg.idle_timeout = rule['idle_timeout']
+      msg.match.dl_dst = rule['dl_dst'] if rule['dl_dst'] else None
+      msg.match.dl_type = rule['dl_type'] if rule['dl_type'] else None
+      msg.idle_timeout = rule['idle_timeout'] if rule['idle_timeout'] else None
+      msg.match.nw_proto = rule['nw_proto'] if rule['nw_proto'] else None
       msg.actions.append(of.ofp_action_output(port = rule['action']))
       event.connection.send(msg)
     
