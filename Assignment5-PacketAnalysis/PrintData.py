dfc1fb287d85ef1ddff71f24c2c610e3d5af0361
rigo5632 <rmquiroz5@gmail.com>
2020-11-26 09:54:13 -0700

    cleaned code

--- Assignment5-PacketAnalysis/PrintData.py
+++ Assignment5-PacketAnalysis/PrintData.py
@@ -1,5 +1,6 @@
 def printAverageSize(average):
-    print(f'* Average Size: \t{average}')
+    print(f'* Packet Average Size')
+    print(f'- Packets Average Byte Size:\t{average}')
 
 def printTopPorts(ports, classification, totalPackets):
     print(f'* {classification} (Port Number - Number of Instances - Percentage of Traffic)')
@@ -10,20 +11,22 @@ def printTopPorts(ports, classification, totalPackets):
 def printAddressTraffic(addresses, limit, totalPackets):
     percentage = limit * 100
     limit = int(limit*totalPackets)
-    print(f'* {percentage}% Source Address Traffic (Address - (instances: #, number of bytes: #))')
+    print(f'* {percentage}% Source Address Traffic (Address - instances - bytes)')
     for i in range(limit):
-        print(f'\t{addresses[i][0]}\t-\t{addresses[i][1]}')
+        address, instances, numberOfBytes = addresses[i][0], addresses[i][1]['instances'], addresses[i][1]['numberOfBytes']
+        print(f'\t{address}\t-\t{instances}\t-\t{numberOfBytes}\t\tbytes')
 
 def printMaskTraffic(instances, numberOfBytes, totalPackets):
-    print(f'* Mask 0 Instances: \t{instances}')
-    print(f'* Mask 0 Total Bytes: \t{numberOfBytes}/{totalPackets}')
+    print(f'* Mask 0 Information')
+    print(f'- Instances: {instances}')
+    print(f'- Total Bytes: \t{numberOfBytes}/{totalPackets}')
 
 def printInstituesTraffic(sentByInstitute, sentToInstitute, totalPackets, toatlBytes):
     sentByInstituteBytes, sentByInstitutePackets = sentByInstitute['bytes'], sentByInstitute['packets']
     sentToInstituteBytes, sentToInstitutePackets = sentToInstitute['bytes'], sentToInstitute['packets']
     print(f'* Institute A Traffic')
-    print(f'* Sent By Institue A Bytes: \t{sentByInstituteBytes}/{toatlBytes} \tbytes')
-    print(f'* Sent By Institue A Packets: \t{sentByInstitutePackets}/{totalPackets} \t\tpackets')
-    print(f'* Sent To Institue A Bytes: \t{sentToInstituteBytes}/{toatlBytes} \tbytes')
-    print(f'* Sent To Institue A Packets: \t{sentToInstitutePackets}/{totalPackets} \t\tpackets')
+    print(f'- Sent By Institue A Bytes: \t{sentByInstituteBytes}/{toatlBytes} \tbytes')
+    print(f'- Sent By Institue A Packets: \t{sentByInstitutePackets}/{totalPackets} \t\tpackets')
+    print(f'- Sent To Institue A Bytes: \t{sentToInstituteBytes}/{toatlBytes} \tbytes')
+    print(f'- Sent To Institue A Packets: \t{sentToInstitutePackets}/{totalPackets} \t\tpackets')
 
