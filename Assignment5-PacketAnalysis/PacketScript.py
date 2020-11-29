e706386af962493a4c2570cf13351166b62dbdc0
rigo5632 <rmquiroz5@gmail.com>
2020-11-29 13:15:07 -0700

    Addded Graphs

--- Assignment5-PacketAnalysis/PacketScript.py
+++ Assignment5-PacketAnalysis/PacketScript.py
@@ -1,133 +1,170 @@
 import numpy as np
-import scipy.stats
+import csv
+import math
 import re
 from matplotlib import pyplot as plt
 from PrintData import PrintPacketData
 
 class PacketManager():
     def __init__(self):
-        self.packetFile             = np.genfromtxt('./Packet-Data/Netflow_dataset_small.csv', delimiter=',', dtype=None, encoding=None)
-        self.totalBytes             = 0
+        self.packetFile             = './Packet-Data/Netflow_dataset.csv'
         self.totalPackets           = 0
-        self.averagePacketSize      = 0
-        self.zeroMaskPackets        = 0
-        self.sourcePorts            = {}
-        self.destinationPorts       = {}
-        self.networkTraffic         = {}
-        self.filteredNetworkTraffic = {}
-        self.instituteSource        = {'bytes': 0, 'packets': 0}
-        self.instituteDestination   = {'bytes': 0, 'packets': 0}
-
+        self.totalBytes             = 0
+        self.graphData              = {'durations':[], 'sizes':[]}
+        self.topSourcePorts         = {}
+        self.topDestinationPorts    = {}
+        self.popularHosts           = {}
+        self.zeroMaskHosts          = {'instances': 0, 'totalBytes': 0}
+        self.filteredPopularHosts   = {}
+        self.instituteSource        = {'instances': 0, 'totalBytes': 0, 'totalPackets': 0}
+        self.instituteDestination   = {'instances': 0, 'totalBytes': 0, 'totalPackets': 0}
     
-    def extractData(self):
-        for data in self.packetFile:
-            packets,packetBytes,timeStampStart,timeStampEnd,sourceAddress,destinationAddress = data[4],data[5],data[6],data[7],data[10],data[11]
-            sourcePort,destinationPort,protocol,sourceMask,destinationMask,sourceAutonomous,destinationAutonomous = data[15],data[16],data[17],data[20],data[21],data[22],data[23]
-            self.countTotalPackets(packets)
-            self.countPacketBytes(packetBytes)
-            self.topSourcePorts(sourcePort,packetBytes)
-            self.topDestinationPorts(destinationPort,packetBytes)
-            self.mostActiveHosts(sourceAddress, packetBytes)
-            self.filteredActiveHosts(sourceAddress, packetBytes, sourceMask)
-            self.institutesNetworkTraffic(sourceAddress, destinationAddress, packetBytes, packets)
-        self.calculateAveragePacketSize()
-        self.sortSets()
-    
-    def calculateAveragePacketSize(self):
-        self.averagePacketSize = self.totalBytes/self.totalPackets
-    
-    def sortSets(self):
-        self.sourcePorts = sorted(self.sourcePorts.items(), key=lambda x:x[1]['instances'], reverse=True)
-        self.destinationPorts = sorted(self.destinationPorts.items(), key=lambda x:x[1]['instances'], reverse=True)
-        self.networkTraffic = sorted(self.networkTraffic.items(), key=lambda x:x[1]['instances'], reverse=True)
-        self.filteredNetworkTraffic = sorted(self.filteredNetworkTraffic.items(), key=lambda x:x[1]['instances'], reverse=True)
-
-    def countTotalPackets(self, numberOfPackets): 
-        self.totalPackets += numberOfPackets
-    
-    def countPacketBytes(self, numberOfBytes): 
-        self.totalBytes += numberOfBytes
-
-    def topSourcePorts(self, port, packetBytes):
-        if self.sourcePorts.get(port) == None:
-            self.sourcePorts[port] = {'instances': 1, 'bytes': packetBytes}
-        else:
-            self.sourcePorts[port]['instances'] += 1
-            self.sourcePorts[port]['bytes'] += packetBytes
+    def extractFileData(self):
+        with open(self.packetFile, 'r') as csvFile:
+            csvReader = csv.reader(csvFile, delimiter=',')
+            next(csvReader)
+            for data in csvReader:
+                packets, packetBytes = int(data[4]), int(data[5])
+                self.totalPackets += packets
+                self.totalBytes += packetBytes
+                self.sourcePorts(data[15], packetBytes)
+                self.destinationPorts(data[16], packetBytes)
+                self.activeHosts(data[10], packetBytes)
+                self.recordZeroMaskHost(data[20], packetBytes)
+                self.filterActiveHosts(data[10], packetBytes, data[20])
+                self.instituteInformation(data[10], data[11], packetBytes, packets)
+                self.graphData['durations'].append(int(data[7]) - int(data[6]))
+                self.graphData['sizes'].append(packetBytes/packets)
         
-    def topDestinationPorts(self, port, packetBytes):
-        if self.destinationPorts.get(port) == None:
-            self.destinationPorts[port] = {'instances': 1, 'bytes': packetBytes}
-        else:
-            self.destinationPorts[port]['instances'] += 1
-            self.destinationPorts[port]['bytes'] += packetBytes
+    def generateLinearDurationGraph(self):
+        figure1 = plt.figure(1)
+
+        x = np.sort(self.graphData['durations'])
+        y = 1 - (np.array(range(len(self.graphData['durations'])))/float(len(self.graphData['durations'])))
+
+        plt.title('Linera Duration Graph')
+        plt.plot(x, y)
     
-    def mostActiveHosts(self, ipAddress, packetBytes):
-        if self.networkTraffic.get(ipAddress) == None:
-            self.networkTraffic[ipAddress] = {'instances': 1, 'bytes': packetBytes}
-        else:
-            self.networkTraffic[ipAddress]['instances'] += 1
-            self.networkTraffic[ipAddress]['bytes'] += packetBytes
+    def generateLogDurationsGraph(self):
+        figure2 =  plt.figure(2)
+        sortedX = np.sort(self.graphData['durations'])
+        x = [math.log(sortedX[i]) if sortedX[i] > 0 else 0 for i in range(len(sortedX))]
+        y = 1 - (np.array(range(len(x))))/float(len(x))
+
+        plt.title('Log Duration Graph')
+        plt.plot(x, y)
     
-    def filteredActiveHosts(self, ipAddress, packetBytes, mask):
-        if mask == 0:
-            self.zeroMaskPackets += packetBytes
+    def generateLinearSizeGraph(self):
+        figure3 = plt.figure(3)
+
+        x = np.sort(self.graphData['sizes'])
+        y = 1 - (np.array(range(len(self.graphData['sizes']))))/float(len(self.graphData['sizes']))
+
+        plt.title('Linear Size Graph')
+        plt.plot(x, y)
+    
+    def generateLogSizeGraph(self):
+        figure4 = plt.figure(4)
+        sortedX = np.sort(self.graphData['sizes'])
+        x = [math.log(sortedX[i]) if sortedX[i] > 0 else 0 for i in range(len(sortedX))]
+        y = 1 - (np.array(range(len(x))))/float(len(x))
+
+        plt.title('Log Size Graph')
+        plt.plot(x, y)
+            
+    def averagePacketSize(self): 
+        return self.totalBytes / self.totalPackets
+    
+    def sourcePorts(self, port, packetBytes):
+        if self.topSourcePorts.get(port) == None:
+            self.topSourcePorts[port] = {'instances': 1, 'totalBytes': packetBytes}
             return
         else:
-            if self.filteredNetworkTraffic.get(ipAddress) == None:
-                self.filteredNetworkTraffic[ipAddress] = {'instances': 1, 'bytes': packetBytes}
-            else:
-                self.filteredNetworkTraffic[ipAddress]['instances'] += 1
-                self.filteredNetworkTraffic[ipAddress]['bytes'] += packetBytes
+            self.topSourcePorts[port]['instances'] += 1
+            self.topSourcePorts[port]['totalBytes'] += packetBytes
+        return
     
-    def institutesNetworkTraffic(self, source, destination, packetBytes, packets):
-        minAddress = [128, 112, 0, 0]
-        maxAddress = [128, 112, 255, 255]
-
-        sourceAddress, destinationAddress = self.convertAddress(source, destination)
-
-        if minAddress[0] == sourceAddress[0] and minAddress[1] == sourceAddress[1]:
-            if sourceAddress[2] >= minAddress[2] and sourceAddress[3] >= minAddress[3] and sourceAddress[2] <= maxAddress[2] and sourceAddress[3] <= maxAddress[3]:
-                self.instituteSource['bytes'] += packetBytes
-                self.instituteSource['packets'] += packets
-
-
-        if minAddress[0] ==  destinationAddress[0] and minAddress[1] ==  destinationAddress[1]:
-            if  destinationAddress[2] >= minAddress[2] and  destinationAddress[3] >= minAddress[3] and  destinationAddress[2] <= maxAddress[2] and  destinationAddress[3] <= maxAddress[3]:
-                self.instituteDestination['bytes'] += packetBytes
-                self.instituteDestination['packets'] += packets
-
+    def destinationPorts(self, port, packetBytes):
+        if self.topDestinationPorts.get(port) == None:
+            self.topDestinationPorts[port] = {'instances': 1, 'totalBytes': packetBytes}
+            return
+        else:
+            self.topDestinationPorts[port]['instances'] += 1
+            self.topDestinationPorts[port]['totalBytes'] += packetBytes
+        return
     
-    def convertAddress(self, sourceAddress, destinationAddress):
-        sourceAddress = re.split('\.', sourceAddress)
-        destinationAddress = re.split('\.', destinationAddress)
+    def activeHosts(self, ipAddress, packetBytes):
+        if self.popularHosts.get(ipAddress) == None:
+            self.popularHosts[ipAddress] = {'instances': 1, 'totalBytes': packetBytes}
+            return
+        else:
+            self.popularHosts[ipAddress]['instances'] += 1
+            self.popularHosts[ipAddress]['totalBytes'] += packetBytes
+        return
+    
+    def recordZeroMaskHost(self, mask, packetBytes):
+        if mask == 0:
+            self.zeroMaskHosts['instances'] += 1
+            self.zeroMaskHosts['totalBytes'] += packetBytes
+        return
+    
+    def filterActiveHosts(self, ipAddress, packetBytes, mask):
+        if mask == 0:
+            return
+        if self.filteredPopularHosts.get(ipAddress) == None:
+            self.filteredPopularHosts[ipAddress] = {'instances': 1, 'totalBytes': packetBytes}
+        else:
+            self.filteredPopularHosts[ipAddress]['instances'] += 1
+            self.filteredPopularHosts[ipAddress]['totalBytes'] += packetBytes
+    
+    def instituteInformation(self, sourceAddress, destinationAddress, packetBytes, packets):
+        source, destination = self.extractAddress(sourceAddress, destinationAddress)
+
+        if source[0] == 128 and source[1] == 112 and source[2] >= 0 and source[3] >= 0 and source[2] <= 255 and source[3] <= 255:
+            self.instituteSource['instances'] += 1
+            self.instituteSource['totalBytes'] += packetBytes
+            self.instituteSource['totalPackets'] += packets
+
+        if destination[0] == 128 and destination[1] == 112 and destination[2] >= 0 and destination[3] >= 0 and destination[2] <= 255 and destination[3] <= 255:
+            self.instituteDestination['instances'] += 1
+            self.instituteDestination['totalBytes'] += packetBytes
+            self.instituteDestination['totalPackets'] += packets
+
+    def extractAddress(self, source, destination):
+        sourceAddress = re.split('\.', source)
+        destinationAddress = re.split('\.', destination)
 
         for i in range(4):
             sourceAddress[i] = int(sourceAddress[i])
             destinationAddress[i] = int(destinationAddress[i])
         
         return sourceAddress, destinationAddress
+    
+    def sortTopPorts(self):
+        return (sorted(self.topSourcePorts.items(), key=lambda x:x[1]['totalBytes'], reverse=True),
+        sorted(self.topDestinationPorts.items(), key=lambda x:x[1]['totalBytes'], reverse=True),
+        sorted(self.popularHosts.items(), key=lambda x:x[1]['instances'], reverse=True),
+        sorted(self.filteredPopularHosts.items(), key=lambda x:x[1]['instances'], reverse=True))
 
-     
+
 analysis = PacketManager()
 packet = PrintPacketData()
-analysis.extractData()
+analysis.extractFileData()
+analysis.generateLinearDurationGraph()
+analysis.generateLogDurationsGraph()
+analysis.generateLinearSizeGraph()
+analysis.generateLogSizeGraph()
+sortedSourcePorts, sortedDestinationPorts, sortedPopularHosts, sortedFilteredHosts = analysis.sortTopPorts()
 
-packet.printAverageSize(analysis.averagePacketSize)
-packet.printTopPorts(analysis.sourcePorts, 'Source Ports', analysis.totalPackets, analysis.totalBytes)
-packet.printTopPorts(analysis.destinationPorts, 'Destination Ports', analysis.totalPackets, analysis.totalBytes)
-packet.printAddressTraffic(analysis.networkTraffic, 0.001, analysis.totalPackets, analysis.totalBytes, 1)
-packet.printAddressTraffic(analysis.networkTraffic, 0.01, analysis.totalPackets, analysis.totalBytes, 2)
-packet.printAddressTraffic(analysis.networkTraffic, 0.1, analysis.totalPackets, analysis.totalBytes, 3)
-packet.printMaskTraffic(analysis.zeroMaskPackets, analysis.totalBytes)
-packet.printAddressTraffic(analysis.filteredNetworkTraffic, 0.001, analysis.totalPackets, analysis.totalBytes, 5.1)
-packet.printAddressTraffic(analysis.filteredNetworkTraffic, 0.01, analysis.totalPackets, analysis.totalBytes, 5.2)
-packet.printAddressTraffic(analysis.filteredNetworkTraffic, 0.1, analysis.totalPackets, analysis.totalBytes, 5.3)
-packet.printInstitutesTraffic(analysis.instituteSource, analysis.instituteDestination, analysis.totalPackets, analysis.totalBytes)
-
-
-
-
-
-
-
+packet.printAveragePacketSize(analysis.averagePacketSize())
+packet.printTopPorts('Source Ports', sortedSourcePorts, analysis.totalPackets, analysis.totalBytes)
+packet.printTopPorts('Destination Ports', sortedDestinationPorts, analysis.totalPackets, analysis.totalBytes)
+packet.printAddressTraffic(sortedPopularHosts, 0.001, analysis.totalBytes)
+packet.printAddressTraffic(sortedPopularHosts, 0.01, analysis.totalBytes)
+packet.printAddressTraffic(sortedPopularHosts, 0.1, analysis.totalBytes)
+packet.printZeroMaskTraffic(analysis.zeroMaskHosts, analysis.totalBytes)
+packet.printAddressTraffic(sortedFilteredHosts, 0.001, analysis.totalBytes)
+packet.printAddressTraffic(sortedFilteredHosts, 0.01, analysis.totalBytes)
+packet.printAddressTraffic(sortedFilteredHosts, 0.1, analysis.totalBytes)
+packet.printInstituteTraffic(analysis.instituteSource, analysis.instituteDestination, analysis.totalPackets, analysis.totalBytes)
+plt.show()
--- Assignment5-PacketAnalysis/PrintData.py
+++ Assignment5-PacketAnalysis/PrintData.py
@@ -1,37 +1,34 @@
-# Prints Packet Data
 class PrintPacketData():
-    def printAverageSize(self, average):
-        print(f'A. Packet Average Size')
-        print(f'* Packets Average Byte Size:\t{average:.2f}')
-
-    def printTopPorts(self, ports, classification, totalPackets, totalBytes):
-        print(f'C. {classification} (Port Number - Packet Bytes per Total Packets - Packet Bytes per Total Bytes)')
+    def printAveragePacketSize(self, average):
+        print(f'A. Packet Average Size: {average:.2f} bytes/packet')
+    
+    def printTopPorts(self, type, ports, totalPackets, totalBytes):
+        print(f'C. Top 10 {type} (Port Number - Total Bytes Percentage)')
         for i in range(10):
-            port, instances, packetBytes = ports[i][0], ports[i][1]['instances'], ports[i][1]['bytes']
-            bytesByTotalPackets = (packetBytes/totalPackets)*100
-            bytesByTotalBytes =  (packetBytes/totalBytes)*100
-            print(f'\t{port}\t-\t{bytesByTotalBytes:.2f}%\t-\t{bytesByTotalBytes:.2f}%')
-
-    def printAddressTraffic(self, addresses, limit, totalPackets, totalBytes, section):
+            port, instances, packetBytes = ports[i][0], ports[i][1]['instances'], ports[i][1]['totalBytes']
+            totalPacketsPercentage, totalBytesPercentage = (instances/totalPackets)*100, (packetBytes/totalBytes)*100
+            print(f'\t{port}\t-\t{totalBytesPercentage:.2f}%')
+    
+    def printAddressTraffic(self, addresses, limit, totalBytes):
         percentage = limit * 100
-        limit = int(limit*totalPackets)
+        limit = int(limit*len(addresses))
         totalTraffic = 0
-        for i in range(limit): 
-            if i >= len(addresses):break
-            totalTraffic += addresses[i][1]['bytes']
-        print(f'C.{section} {percentage}% Source Address Traffic')
+
+        for i in range(limit): totalTraffic += addresses[i][1]['totalBytes']
+
+        print(f'D. {percentage}% Source Address Traffic')
         print(f'* Total Traffic: \t{totalTraffic} bytes / {totalBytes} bytes')
-
-    def printMaskTraffic(self, numberOfBytes, totalPackets):
-        print(f'C.4 Mask 0 Information')
-        print(f'* Total Bytes: \t{numberOfBytes} bytes / {totalPackets} bytes')
-
-    def printInstitutesTraffic(self, sentByInstitute, sentToInstitute, totalPackets, toatlBytes):
-        sentByInstituteBytes, sentByInstitutePackets = sentByInstitute['bytes'], sentByInstitute['packets']
-        sentToInstituteBytes, sentToInstitutePackets = sentToInstitute['bytes'], sentToInstitute['packets']
-        print(f'* Institute A Traffic')
-        print(f'- Sent By Institue A Bytes: \t{sentByInstituteBytes}/{toatlBytes} \tbytes')
-        print(f'- Sent By Institue A Packets: \t{sentByInstitutePackets}/{totalPackets} \t\tpackets')
-        print(f'- Sent To Institue A Bytes: \t{sentToInstituteBytes}/{toatlBytes} \tbytes')
-        print(f'- Sent To Institue A Packets: \t{sentToInstitutePackets}/{totalPackets} \t\tpackets')
-
+    
+    def printZeroMaskTraffic(self, mask, totalBytes):
+        maskBytes = mask['totalBytes']
+        print(f'D. Mask 0 Information')
+        print(f'* Total Bytes: \t{maskBytes} bytes / {totalBytes} bytes')
+    
+    def printInstituteTraffic(self, sentByInstitute, sentToInstitute, totalPackets, totalBytes):
+        sentByBytes, sentByPackets = sentByInstitute['totalBytes'], sentByInstitute['totalPackets']
+        sentToBytes, sentToPackets = sentToInstitute['totalBytes'], sentToInstitute['totalPackets']
+        print('E. Institute A Traffic')
+        print(f'* Sent By Institue A Bytes: \t{sentByBytes} / {totalBytes} \tbytes')
+        print(f'* Sent By Institue A Packets: \t{sentByPackets} / {totalPackets} \t\tpackets')
+        print(f'* Sent To Institue A Bytes: \t{sentToBytes} / {totalBytes} \tbytes')
+        print(f'* Sent To Institue A Packets: \t{sentToPackets} / {totalPackets} \t\tpackets')
